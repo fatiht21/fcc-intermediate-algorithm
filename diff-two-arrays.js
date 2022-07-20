f68587e53150f7484504497448a9247e3306e26e
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-07-20 13:36:38 +0200

    diff-two-arrays s2

--- diff-two-arrays.js
+++ diff-two-arrays.js
@@ -29,3 +29,21 @@ console.log(
     ["diorite", "andesite", "grass", "dirt", "dead shrub"]
   )
 );
+
+//s2
+function diffArray(arr1, arr2) {
+  const newArr = [];
+
+  function onlyInFirst(first, second) {
+    for (let i = 0; i < first.length; i++) {
+      if (second.indexOf(first[i]) === -1) {
+        newArr.push(first[i]);
+      }
+    }
+  }
+
+  onlyInFirst(arr1, arr2);
+  onlyInFirst(arr2, arr1);
+
+  return newArr;
+}
