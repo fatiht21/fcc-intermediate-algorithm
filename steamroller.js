8d7fc37612f61bc41cbbccb9e54a3cbb4943db7e
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-07-27 09:58:13 +0200

    steamroller

---
+++ steamroller.js
@@ -0,0 +1,19 @@
+/*
+Flatten a nested array. You must account for varying levels of nesting.
+
+steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"].
+steamrollArray([1, [2], [3, [[4]]]]) should return [1, 2, 3, 4].
+steamrollArray([1, [], [3, [[4]]]]) should return [1, 3, 4].
+steamrollArray([1, {}, [3, [[4]]]]) should return [1, {}, 3, 4].
+Your solution should not use the Array.prototype.flat() or Array.prototype.flatMap() methods.
+*/
+
+function steamrollArray(arr) {
+  let flatArr = [];
+  arr.map((x) =>
+    !Array.isArray(x) ? flatArr.push(x) : flatArr.push(...steamrollArray(x))
+  );
+  return flatArr;
+}
+
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
